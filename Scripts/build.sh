83ce1b98325b63f4d4a872aabc17399a852240ab
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-08 01:13:12 +0200

    Logging during build is now done in the console directly

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -7,7 +7,6 @@ echo "Attempting to build $project for Windows"
   -batchmode 
   -nographics 
   -silent-crashes 
-  -logFile $(pwd)/unity.log 
   -projectPath $(pwd) 
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
   -quit
@@ -17,7 +16,6 @@ echo "Attempting to build $project for Windows"
 #  -batchmode 
 #  -nographics 
 #  -silent-crashes 
-#  -logFile $(pwd)/unity.log 
 #  -projectPath $(pwd) 
 #  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
 #  -quit
@@ -27,13 +25,9 @@ echo "Attempting to build $project for Windows"
 #  -batchmode 
 #  -nographics 
 #  -silent-crashes 
-#  -logFile $(pwd)/unity.log 
 #  -projectPath $(pwd) 
 #  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
 #  -quit
-
-echo 'Logs from build'
-cat $(pwd)/unity.log
 
 echo 'Attempting to zip builds'
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
