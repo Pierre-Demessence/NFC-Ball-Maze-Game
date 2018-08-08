4d327ffdef0b3a1be1fb6b84d776189bb02d4157
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-08 03:16:14 +0200

    Arguments on one line
    because it seems to not work otherwise

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,24 +3,10 @@
 project="NFC-Ball-Maze-Game"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -projectPath $(pwd) -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" -quit
 
 #echo "Attempting to build $project for OS X"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-#  -batchmode \
-#  -nographics \
-#  -silent-crashes \
-#  -logFile $(pwd)/unity.log \
-#  -projectPath $(pwd) \
-#  -buildOSXPlayer "$(pwd)/Build/osx/$project.app" \
-#  -quit
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath $(pwd) -buildOSXPlayer "$(pwd)/Build/osx/$project.app" -quit
 
 echo 'Attempting to zip builds'
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
