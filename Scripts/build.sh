813e767a5c3945a1c935216cd1de6683fb9ed291
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-08 03:05:46 +0200

    Added \ at the end of lines to concat parameters...

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,27 +3,24 @@
 project="NFC-Ball-Maze-Game"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildOSXPlayer "$(pwd)/Build/osx/$project.app" 
-  -quit
-
-echo 'Logs from build'
-cat $(pwd)/unity.log
+#echo "Attempting to build $project for OS X"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+#  -batchmode \
+#  -nographics \
+#  -silent-crashes \
+#  -logFile $(pwd)/unity.log \
+#  -projectPath $(pwd) \
+#  -buildOSXPlayer "$(pwd)/Build/osx/$project.app" \
+#  -quit
 
 echo 'Attempting to zip builds'
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
