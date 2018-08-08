6556c6010559e4b908b30293e235d78b1c334309
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-08 03:24:20 +0200

    Readded -logfile with no parameter

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,7 +3,7 @@
 project="NFC-Ball-Maze-Game"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -projectPath $(pwd) -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile -projectPath $(pwd) -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" -quit
 
 #echo "Attempting to build $project for OS X"
 #/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath $(pwd) -buildOSXPlayer "$(pwd)/Build/osx/$project.app" -quit
