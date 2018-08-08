848df140c17094e2ae0a4dbe94cec92dc3b66f4e
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-08 23:50:51 +0200

    Added and Fixed Travis CI (#3)
    * Added more drag to the test ball
    
    * Moved the scripts into a "Scripts" directory
    
    * Fixed comments
    
    * Should have fixed the CI
    
    * Logging during build is now done in the console directly
    
    * Re-added -logFile but without the path so it outputs to the console
    
    * Another try to get the editor log into the console for Travis CI
    
    * Added touch before calling tail on the non-existing file (flm)
    
    * Add more time to the build because no logs are shown
    
    Also changed build from Windows to Linux, if that helps
    
    * Added \ at the end of lines to concat parameters...
    
    * Arguments on one line
    
    because it seems to not work otherwise
    
    * Readded -logfile with no parameter
    
    * Re-added everything in multiple line because it worked
    
    * Added credentials
    
    * Let's try with 2017.2.0f3
    
    * Removed Mono
    
    * Updated to 2017.4.8f1
    
    * Added TestBench to BuildSettings.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 BASE_URL=https://netstorage.unity3d.com/unity
-HASH=c18cef34cbcd
-VERSION=2018.2.2f1
+HASH=5ab7f4878ef1
+VERSION=2017.4.8f1
 
 download() {
   file=$1
@@ -25,5 +25,6 @@ install() {
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
-//install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
-//install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
+#install "MacEditorTargetInstaller/UnitySetup-Mac-IL2CPP-Support-for-Editor-$VERSION.pkg"
+#install "MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-$VERSION.pkg"
+#install "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg"
