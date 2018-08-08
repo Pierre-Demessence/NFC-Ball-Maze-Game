c7da450ecb72805afa27fef38b344b14072693bc
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-08 02:58:12 +0200

    Add more time to the build because no logs are shown
    Also changed build from Windows to Linux, if that helps

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,9 +2,8 @@
 
 project="NFC-Ball-Maze-Game"
 
-touch $(pwd)/unity.log
 echo "Attempting to build $project for Windows"
-tail -F $(pwd)/unity.log & /Applications/Unity/Unity.app/Contents/MacOS/Unity 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
   -batchmode 
   -nographics 
   -silent-crashes 
@@ -12,33 +11,19 @@ tail -F $(pwd)/unity.log & /Applications/Unity/Unity.app/Contents/MacOS/Unity
   -projectPath $(pwd) 
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
   -quit
-kill %1
 
-#touch $(pwd)/unity.log
-#echo "Attempting to build $project for OS X"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-#  -batchmode 
-#  -nographics 
-#  -silent-crashes 
-#  -logFile $(pwd)/unity.log 
-#  -projectPath $(pwd) 
-#  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
-#  -quit
-#kill %1
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildOSXPlayer "$(pwd)/Build/osx/$project.app" 
+  -quit
 
-#touch $(pwd)/unity.log
-#echo "Attempting to build $project for Linux"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-#  -batchmode 
-#  -nographics 
-#  -silent-crashes 
-#  -logFile $(pwd)/unity.log 
-#  -projectPath $(pwd) 
-#  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
-#  -quit
-#kill %1
+echo 'Logs from build'
+cat $(pwd)/unity.log
 
 echo 'Attempting to zip builds'
 zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
-#zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
-#zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
