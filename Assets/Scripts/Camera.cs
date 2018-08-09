25bdea62e8dd8c0214aed16c27654870b26c00b0
Naxane <conor.lawton1@gmail.com>
2018-08-09 17:07:31 +0100

    Created example maze
    + Added materials for glass and wood
    + Created camera script that follows ball

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject Ball;
+    public GameObject Maze;
+    private Vector3 _averagePos;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        _averagePos = (Ball.transform.position + Maze.transform.position) / 2;
+        transform.position = new Vector3(_averagePos.x, _averagePos.y+10, _averagePos.z-5);
+
+    }
+}
