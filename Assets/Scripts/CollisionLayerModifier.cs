a55ab27a8acd22ca3cf31e7843191aceb47e94b6
Unknown <camblomquist@gmail.com>
2018-08-09 11:14:15 -0400

    Add Win Condition
    Full Feature List:
    A "Holdout" shader, Acts similar to the one of the same name in Blender.
    A script pair that changes the render order of any material or shader at runtime or edit mode.
    A script pair that can change the object's collision layer on hitting a trigger
    The goal script. Super basic right now, only checks for a tagged object on trigger and prints to the console, but has an event to easily expand functionality
    A prefab to easily drag and drop the goal hole into a scene
    A set of basic color materials for easy testing and to avoid the blinding white of the default material
    A steel material to make the ball look nice
    Upped the gravity to -15. Feels better, but can still be changed.

---
+++ Assets/Scripts/CollisionLayerModifier.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: If needed, add a way to store the original object's layer and set it back to the object on exit
+// 		 If all the objects end up needing to exit onto the same layer, this task is unnecessary
+public class CollisionLayerModifier : MonoBehaviour
+{
+
+	[SerializeField, Tooltip("If non-empty, will only modify objects with a tag in this list")] 
+	private string[] _validTags;
+	[SerializeField, Tooltip("The layer that the colliding object will move to when entering the trigger")] 
+	private int _enterLayer;
+	[SerializeField, Tooltip("The layer that the colliding object will move to when exiting the trigger")] 
+	private int _exitLayer;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (_validTags.Length > 0)
+		{
+			foreach (var tag in _validTags)
+			{
+				if (other.gameObject.CompareTag(tag))
+				{
+					other.gameObject.layer = _enterLayer;
+					return; // No reason to continue the search
+				}
+			}
+		}
+		else
+		{
+			other.gameObject.layer = _enterLayer;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (_validTags.Length > 0)
+		{
+			foreach (var tag in _validTags)
+			{
+				if (other.gameObject.CompareTag(tag))
+				{
+					other.gameObject.layer = _exitLayer;
+					return; // No reason to continue the search
+				}
+			}
+		}
+		else
+		{
+			other.gameObject.layer = _exitLayer;
+		}
+	}
+}
