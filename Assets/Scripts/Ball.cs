bd23a80ea36239c59b8a2c0af4af76ad29714980
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-10 13:39:05 +0200

    Added a sound when the ball roll

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	[SerializeField] float pFactor = 1f;
+	[SerializeField] float vFactor = 1f;
+	[SerializeField] AudioSource _aSource;
+	[SerializeField] Rigidbody _rBody;
+	
+	// Update is called once per frame
+	private void Update () {
+		Debug.Log(_rBody.maxAngularVelocity);
+		Debug.Log(_rBody.velocity.magnitude);
+		float factor = _rBody.velocity.magnitude / _rBody.maxAngularVelocity;
+		_aSource.pitch = factor / pFactor;
+		_aSource.volume = factor / vFactor;
+	}
+}
