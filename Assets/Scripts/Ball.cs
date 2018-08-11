0fd2a7a489ec8009df62642b05ccae0cdb680d3c
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-11 16:59:34 +0200

    Added collision sound with the walls

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,17 +2,37 @@
 
 public class Ball : MonoBehaviour {
 
-	[SerializeField] float pFactor = 1f;
-	[SerializeField] float vFactor = 1f;
-	[SerializeField] AudioSource _aSource;
-	[SerializeField] Rigidbody _rBody;
+	[SerializeField] private Rigidbody _rBody;
 	
+	[SerializeField] private AudioSource _sfxRolling;
+	[SerializeField] private float _sfxRollingPitchFactor = 1f;
+	[SerializeField] private float  _sfxRollingVolumeFactor = 1f;
+	
+	[SerializeField] private AudioSource _sfxColliding;
+	[SerializeField] private float _sfxCollidingPitchMin = 0.5f;
+	[SerializeField] private float _sfxCollidingPitchMax = 1;
+	[SerializeField] private float _sfxCollidingVolumeMin = 0;
+	[SerializeField] private float _sfxCollidingVolumeMax = 0.5f;
+	
+
 	// Update is called once per frame
 	private void Update () {
 		//Debug.Log(_rBody.maxAngularVelocity);
 		//Debug.Log(_rBody.velocity.magnitude);
 		float factor = _rBody.velocity.magnitude / _rBody.maxAngularVelocity;
-		_aSource.pitch = factor / pFactor;
-		_aSource.volume = factor / vFactor;
+		_sfxRolling.pitch = factor / _sfxRollingPitchFactor;
+		_sfxRolling.volume = factor / _sfxRollingVolumeFactor;
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		// Checking the layer containing the walls
+		if (other.collider.gameObject.layer == 10)
+		{
+			float factor = other.impulse.magnitude / 5;
+			_sfxColliding.pitch = Mathf.Lerp(_sfxCollidingPitchMin, _sfxCollidingPitchMax, factor);
+			_sfxColliding.volume = Mathf.Lerp(_sfxCollidingVolumeMin, _sfxCollidingVolumeMax, factor);
+			_sfxColliding.Play();
+		}
 	}
 }
