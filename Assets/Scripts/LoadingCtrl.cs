caa32d1b4e1b511c7f9e7934c31ce7e26f682a4c
Pierre Demessence <pierre.demessence@gmail.com>
2018-08-25 18:03:49 +0200

    Added random messages for LoadingScreen

--- Assets/Scripts/LoadingCtrl.cs
+++ Assets/Scripts/LoadingCtrl.cs
@@ -1,24 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class LoadingCtrl : MonoBehaviour
 {
+    private static readonly string[] _loadingRandomMessages =
+    {
+        "Reticulating Splines...",
+        "Polishing the edges...",
+        "Adding some qubits...",
+        "Adding gravity...",
+        "Extruding the meshes..."
+    };
     private AsyncOperation _asyncOperation;
-    
-    [SerializeField] private bool _touchToFinish;
-    
-    [SerializeField] private Slider _progressBar;
-    [SerializeField] private TMP_Text _text;
+    [SerializeField] private TMP_Text _loadingMessage;
 
-    // Use this for initialization
+    [SerializeField] private Slider _progressBar;
+
+    [SerializeField] private bool _touchToFinish;
+
     private void Start()
     {
         _asyncOperation = SceneManager.LoadSceneAsync("Game");
         _asyncOperation.allowSceneActivation = !_touchToFinish;
         StartCoroutine(CheckProgress());
+        StartCoroutine(ChangeText());
     }
 
     private IEnumerator CheckProgress()
@@ -28,16 +39,21 @@ public class LoadingCtrl : MonoBehaviour
             _progressBar.value = _asyncOperation.progress;
             if (_touchToFinish && _asyncOperation.progress >= 0.9f)
             {
-                _text.text = "Touch to continue!";
+                _loadingMessage.text = "Touch to continue!";
                 _progressBar.value = 1f;
-                if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
-                {
-                    _asyncOperation.allowSceneActivation = true;
-                }
+                if (Input.touchCount > 0 || Input.GetMouseButtonDown(0)) _asyncOperation.allowSceneActivation = true;
             }
 
             yield return null;
         }
+    }
 
+    private IEnumerator ChangeText()
+    {
+        while (!_asyncOperation.isDone)
+        {
+            _loadingMessage.text = _loadingRandomMessages.OrderBy(a => Guid.NewGuid()).First();
+            yield return new WaitForSecondsRealtime(Random.value + 2);
+        }
     }
 }
